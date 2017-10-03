8e994b45ed78429cce260655b0be3646c0a1dec8
Aleksey Gribov <gribov_an@open.ru>
2017-10-03 16:14:06 +0300

    create project

---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Номос Номосенко on 03.10.17.
+//  Copyright © 2017 Номос Номосенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
